1ac30cd36954ac58e858cb09ca1e29f778fdb6ad
Hamza Schischkin <hamza@az10.de>
2023-07-11 15:39:08 +0200

    feat: add splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import './App.css';
 import {gsap} from 'gsap';
 
 function App() {
   useEffect(() => {
-    gsap.to('h1', {
+    const timeline = gsap.timeline();
+    timeline.to('.splash h1', {
       top: 0,
       opacity: 1,
+      duration: 4,
       ease: 'power4.out',
-      stagger: 0.5,
+    }).to('.splash', {
+      top: '-100%',
+      ease: 'power4.out',
+      duration: 2
     });
   }, []);
   return (
     <>
-      <div className="header bg-[#0a0a0a] w-full h-full fixed flex items-center justify-center text-white flex-col">
-        <h1 className="text-6xl font-bold relative">Any fool can write code</h1>
-        <h1 className="text-6xl font-bold relative">that a computer can</h1>
-        <h1 className="text-6xl font-bold relative">understand.</h1>
-        <h1 className="text-6xl font-bold relative">Good programmers</h1>
-        <h1 className="text-6xl font-bold relative">write code that</h1>
-        <h1 className="text-6xl font-bold relative">humans can understand.</h1>
+      <div className="splash bg-[#0a0a0a] w-full h-full fixed flex items-center justify-center text-white flex-col">
+        <h1 className="text-6xl font-bold relative uppercase">Hamza</h1>
       </div>
     </>
   );
